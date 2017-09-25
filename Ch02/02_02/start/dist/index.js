611325f3d959228cf017f42f00f28a9a00ab4c76
github_prukia <rukia.sm.23@gmail.com>
2017-09-25 08:48:10 -0500

    coverted to jsx

--- Ch02/02_02/start/dist/index.js
+++ Ch02/02_02/start/dist/index.js
@@ -1,13 +1,25 @@
-const { createElement } = React
+// const { createElement } = React
 const { render } = ReactDOM
 
-const title = createElement(
-	'h1',
-	{id: 'title', className: 'header'},
-	'Hello World'
-)
+//nested within the style component
+// const style = {
+//   backgroundColor: 'orange',
+// 	color: 'white',
+// 	fontFamily: 'verdana'
+// }
+
+// now this is converted to jsx
+// const title = createElement(
+// 	'h1',
+// 	{id: 'title', className: 'header', style: style},
+// 	'Hello World'
+// )
 
 render(
-	title,
+	<h1 id='title'
+		className='header'
+		style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
+		Hello World
+	</h1>,
 	document.getElementById('react-container')
 )
